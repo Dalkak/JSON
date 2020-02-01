120dc0342db1c03181a541403c9f48c47445b9c6
Gnlow <entrydark@gmail.com>
2020-02-01 17:22:56 +0900

    update dalkak

--- index.ts
+++ index.ts
@@ -2,57 +2,43 @@ import {
     Pack, Block, Literal, Type, Dict
 } from "dalkak";
 
-export default new Pack(
-    new Dict, 
-    "JSON",
-    {
-        parse: new Block(
-            new Dict, 
-            "parse",
-            "(json)해석하고 변수(val)에 담기",
-            (param, info) => {
-                var Entry = (info as {
-                    data
-                }).data.Entry;
-                if (!Entry.variableContainer.getVariableByName(param.val)) {
-                    Entry.variableContainer.appendVariables([{
-                        name: param.val
-                    }]);
-                }
-                Entry.variableContainer.getVariableByName(param.val).setValue(JSON.parse(param.json));
+export default new Pack({
+    name: "JSON",
+    blocks: {
+        parse: new Block({
+            name: "parse",
+            template: "(json) 해석하고 변수 (val)에 담기",
+            func: (param, project) => {
+                var {
+                    json = "", 
+                    val = "변수"
+                } = {...param};
+                project.variables.value[val].value = JSON.parse(json);
             }
-        ),
-        get: new Block(
-            new Dict, 
-            "get",
-            "변수(input)의(propName)값 구하고 변수(val)에 담기",
-            (param, info) => {
-                var Entry = (info as {
-                    data
-                }).data.Entry;
-                if (!Entry.variableContainer.getVariableByName(param.val)) {
-                    Entry.variableContainer.appendVariables([{
-                        name: param.val
-                    }]);
-                }
-                Entry.variableContainer.getVariableByName(param.val).setValue(Entry.variableContainer.getVariableByName(param.input).getValue()[param.propName]);
+        }),
+        get: new Block({
+            name: "get",
+            template: "변수 (input)의 (propName)값 구하고 변수 (val)에 담기",
+            func: (param, project) => {
+                var {
+                    input = "변수", 
+                    propName = "prop", 
+                    val = "변수"
+                } = {...param};
+                project.variables.value[val].value = project.variables.value[input].value[propName]
             }
-        ),
-        set: new Block(
-            new Dict, 
-            "set",
-            "변수(val)의(propName)값을(data)로 정하기",
-            (param, info) => {
-                var Entry = (info as {
-                    data
-                }).data.Entry;
-                if (!Entry.variableContainer.getVariableByName(param.val)) {
-                    Entry.variableContainer.appendVariables([{
-                        name: param.val
-                    }]);
-                }
-                Entry.variableContainer.getVariableByName(param.val).getValue()[param.propName] = param.data;
+        }),
+        set: new Block({
+            name: "set",
+            template: "변수 (val)의 (propName)값을 (data)로 정하기",
+            func: (param, project) => {
+                var {
+                    val = "변수", 
+                    propName = "prop", 
+                    data = ""
+                } = {...param};
+                project.variables.value[val].value[propName] = data;
             }
-        )
+        })
     }
-);
+});
