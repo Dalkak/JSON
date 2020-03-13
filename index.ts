dba328e1e81726c10a9c4399c54a61fc8a0fe053
Gnlow <entrydark@gmail.com>
2020-03-13 11:19:26 +0900

    Use simple syntax

--- index.ts
+++ index.ts
@@ -8,33 +8,21 @@ export default new Pack({
         parse: new Block({
             name: "parse",
             template: "((json) 해석하기)",
-            func: (param, project) => {
-                var {
-                    json = "",
-                } = {...param};
+            func: ({json}) => {
                 return JSON.parse(json);
             }
         }),
         get: new Block({
             name: "get",
             template: "((input)의 (propName)값 구하기)",
-            func: (param, project) => {
-                var {
-                    input = "변수", 
-                    propName = "prop",
-                } = {...param};
+            func: ({input, propName}) => {
                 return input[propName];
             }
         }),
         set: new Block({
             name: "set",
             template: "변수 (val)의 (propName)값을 (data)로 정하기",
-            func: (param, project) => {
-                var {
-                    val = "변수", 
-                    propName = "prop", 
-                    data = ""
-                } = {...param};
+            func: ({val, propName, data}, project) => {
                 project.variables.value[val].value[propName] = data;
             }
         })
